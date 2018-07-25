aa3e31277d8dc630345fdedf6b4c5ae635822add
Taylor Fahlman <tfahlman@redhat.com>
2018-07-25 19:01:10 -0400

    Added save and load functions

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,19 @@ var camels = 0;
 
 function camelClick(number){
     camels = camels + number;
+    console.log("click");
     document.getElementById("camels").innerHTML = camels;
 };
 
+function loadGame() {
+    var savegame = JSON.parse(localStorage.getItem("save"));
+    console.log(savegame);
+    camels = savegame;
+    document.getElementById("camels").innerHTML = camels;
+
+}
+
+function saveGame() {
+    localStorage.setItem("save", JSON.stringify(camels));
+}
 
